5c970cc1063f2f72e518404386cfa510896ddba4
zhangjin <1145839070@qq.com>
2024-07-10 16:52:12 +0800

    更新了

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,11 @@ st.write(df)
 
 st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
-df2 = pd.DataFrame(
-    np.random.randn(200, 3),
-    columns=['a', 'b', 'c']
-)
-c = alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
-)
-st.write(c)
+# df2 = pd.DataFrame(
+#     np.random.randn(200, 3),
+#     columns=['a', 'b', 'c']
+# )
+# c = alt.Chart(df2).mark_circle().encode(
+#     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+# )
+# st.write(c)
